d0f3c10e6813f6dc118dab010d9da2f493f8ae6b
fweeeng <91084706@g.mvusd.net>
2021-01-30 02:24:08 +0000

    Got start button and button 2 to work

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.*;
+//import java.util.ArrayList;
+//import java.util.ListIterator;
 
 class Main 
 {
@@ -8,6 +11,13 @@ class Main
     JFrame frame1 = new JFrame("JFrame here");
     frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //MUST HAVE THIS LINE
     frame1.setSize(500,500);
+    frame1.setVisible(true);
+
+    //-------Panel to hold buttons--------
+    JPanel panel1 = new JPanel(new GridLayout(2,2,20,20));
+
+    //-------Label 1 --------
+
 
     //-------Button1--------
     JButton startButton = new JButton("Start");
@@ -19,8 +29,24 @@ class Main
       }//what the button will do
 
     });//end of ActionListener
+
+    //-------Button2---------
+    JButton button2 = new JButton("Button2");
+    button2.addActionListener(new ActionListener()
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        System.out.println("This is button2.");
+      }
+    });
     frame1.add(startButton);
     frame1.setVisible(true);
+
+    panel1.add(startButton);
+    panel1.add(button2);
+
+    frame1.getContentPane().add(BorderLayout.NORTH,panel1);
+    //JPanel panel2 = new JPanel(new BorderLayout(10,10));
 
   }//end main method
 }//end class Main
